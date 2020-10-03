ab9cc1b8353951e0a093eed995b83c853fd30622
microcheese <ernst.reiter@microcheese.de>
2020-10-03 14:28:22 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let package = Package(
   name: "QRCodeReader",
+  platforms: [.iOS(.v9)],
   products: [
     .library(name: "QRCodeReader", targets: ["QRCodeReader"]),
   ],
